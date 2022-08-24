672448d1e915e1203b84ecdaf398d7f1ab639469
Jamall Malik <mimsjamall79@gmail.com>
2022-08-24 18:42:43 -0400

    finished lab

--- snacks/urls.py
+++ snacks/urls.py
@@ -2,6 +2,6 @@ from .views import HomePageView, AboutPageView
 from django.urls import path
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("potato/", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about")
 ]
--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
-# local apps
+    #Local
     "snacks",
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -55,7 +56,7 @@ ROOT_URLCONF = "snacks_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
