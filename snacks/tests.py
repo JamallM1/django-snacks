7f3ee8320349597c0d4b292e72f7b32ae14f39a3
Jamall Malik <mimsjamall79@gmail.com>
2022-10-02 22:35:08 -0400

    finished lab

--- snacks/tests.py
+++ snacks/tests.py
@@ -13,3 +13,16 @@ class SnacksTests(SimpleTestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'home.html')
         self.assertTemplateUsed(response, 'base.html')
+
+
+    def test_list_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+
+    def test_detail_page_status_code(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
